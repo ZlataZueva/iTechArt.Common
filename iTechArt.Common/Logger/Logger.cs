c64b11bc329b54f4f6201dd6f7d7296e6c108496
Zlata <zlatka.zueva@gmail.com>
2019-08-08 13:32:13 +0300

    Create ILogger interface and implement it by Logger using log4net

---
+++ iTechArt.Common/Logger/Logger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using iTechArt.Common.Interfaces;
+using log4net;
+using log4net.Appender;
+using log4net.Config;
+
+namespace iTechArt.Common.Logger
+{
+    public class Logger : ILogger
+    {
+        private readonly ILog _log;
+
+
+        public Logger(Type type)
+        {
+            _log = LogManager.GetLogger(type);
+        }
+
+
+        public void Debug(string message)
+        {
+            _log.Debug(message);
+        }
+
+        public void Info(string message)
+        {
+            _log.Info(message);
+        }
+
+        public void Warn(string message)
+        {
+            _log.Warn(message);
+        }
+
+        public void Warn(string message, Exception e)
+        {
+            _log.Warn(message, e);
+        }
+
+        public void Error(string message)
+        {
+            _log.Error(message);
+        }
+
+        public void Error(string message, Exception e)
+        {
+            _log.Error(message, e);
+        }
+
+        public void Fatal(string message)
+        {
+            _log.Fatal(message);
+        }
+
+        public void Fatal(string message, Exception e)
+        {
+            _log.Fatal(message, e);
+        }
+    }
+}

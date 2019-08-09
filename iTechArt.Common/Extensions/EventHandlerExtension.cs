ca393f50a1fb209b75ad83f23bacd84607db573a
Zlata <zlatka.zueva@gmail.com>
2019-08-09 13:43:54 +0300

    Add event handler extension and enumerable extension

---
+++ iTechArt.Common/Extensions/EventHandlerExtension.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace iTechArt.Common.Extensions
+{
+    public static class EventHandlerExtension
+    {
+        public static void Raise<TEventArgs> (this EventHandler<TEventArgs> handler, object sender, TEventArgs args)
+            where TEventArgs : EventArgs
+        {
+            handler?.Invoke(sender, args);
+        }
+    }
+}
